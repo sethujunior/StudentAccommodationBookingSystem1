fff824b6e1a101bd25d639cfb1ae5f8f6740fd99
sethujunior <sethumdluli7@gmail.com>
2026-03-23 22:59:59 +0200

    initial base domain code

---
+++ src/main/java/za/ac/cput/domain/ContactDetails.java
@@ -0,0 +1,54 @@
+package za.ac.cput.domain;
+
+public class ContactDetails {
+    private String studentEmail;
+    private String phone;
+
+    public ContactDetails() {
+    }
+
+    //    Builder Constractor
+    public ContactDetails(Builder builder) {
+        this.studentEmail = builder.studentEmail;
+        this.phone = builder.phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactDetails{" +
+                "phone='" + phone + '\'' +
+                ", studentEmail='" + studentEmail + '\'' +
+                '}';
+    }
+    //    Builder Class
+    public static class Builder {
+        private String studentEmail;
+        private String phone;
+
+        public Builder setStudentEmail(String studentEmail) {
+            this.studentEmail = studentEmail;
+            return this;
+        }
+        public Builder ssetPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+        public Builder copy(ContactDetails contactDetails){
+            this.studentEmail = contactDetails.studentEmail;
+            this.phone = contactDetails.phone;
+            return this;
+        }
+        public ContactDetails build(){
+            return new ContactDetails(this);
+        }
+
+    }
+}
